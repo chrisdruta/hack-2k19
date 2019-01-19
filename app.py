340b01df5c9760d5aa2e9a5273b2f0c635c921cc
chris <cndruta@email.arizona.edu>
2019-01-19 06:22:53 -0700

    Changed project directory structure

--- app.py
+++ app.py
@@ -1,24 +1,23 @@
-from flask import Flask
-from flask_restful import Resource, Api
+from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
-
-from resources.user import User
-from resources.device import Device
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/database"
 
-api = Api(app)
-app.mongo = PyMongo(app)
+mongo = PyMongo(app)
 
-class Hi(Resource):
-    def get(self):
-        return {'hi': 'fam'}
+@app.route('/post', methods=['GET', 'POST'])
+def endpoint():
+    
+    body
 
-api.add_resource(Hi, '/')
-api.add_resource(User, '/user')
-api.add_resource(Device, '/device')
-
+    if request.method == 'POST':
+        print(request.get_json())
+        return jsonify({'hello': "world"})
+    else:
+        return jsonify({'hello': "world"})
 
 if __name__ == '__main__':
 	app.run(debug=True, host='0.0.0.0')
+
+@app.route('/')
--- resources/device.py
+++
@@ -1,6 +0,0 @@
-from flask import current_app
-from flask_restful import Resource
-
-class Device(Resource):
-    def get(self):
-        return
