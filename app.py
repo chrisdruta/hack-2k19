a2b67f3b2f3e134894bd41c87f902bb138dae9c2
chris <cndruta@email.arizona.edu>
2019-01-19 18:09:45 -0700

    Added api client and examples

--- app.py
+++ app.py
@@ -2,11 +2,49 @@ from flask import Flask, render_template
 from flask_ask import Ask, statement, question, session
 import face_detect
 from PillDispense import dispenser
+from api_client import APIClient
+import dateutil.parser
+
+from datetime import datetime
 
 app = Flask(__name__)
 ask = Ask(app, "/")
 
-userFound = False
+baseUrl = "insert aws ip here"
+client = APIClient(baseUrl)
+
+# @scott api client example
+# response is a dictionary containing the data you want with arbitrary keys.
+# What ever you data you want to get or update just follow the patterns below.
+
+# Get list of pill logs
+response = client.send_get('pills/logs')
+countPills = 0
+for medicationLog in response['logs']:
+    # Can do date checking
+    datetimeObj = dateutil.parser.parse(medicationLog['date'])
+    countPills += medicationLog['red']
+    countPills += medicationLog['blue']
+
+# Get total pill count
+response = client.send_get('pills/count')
+totalCount = response['count']
+
+# Get specific pill count
+response = client.send_get('pills/count/red')
+redCount = response['count']
+
+# Add a new log dictionary containing data, which also updates pill counts
+client.send_post('pills/logs', {
+    'date': f"{datetime.now().isoformat()}Z",
+    'pills': {
+        'red': 1,
+        'blue': 1
+    }
+})
+
+# Still need to plan prescriptions
+client.get_post('prescription')
 
 # ask: opens on launch of app
 @ask.launch
@@ -18,9 +56,13 @@ def launch_dispense():
 @ask.intent("welcome_user")
 def welcome_user(username):
     # INSERT FUNCTION TO CHECK USER NAME
+    # client.username = None
+    # response = client.send_post('account', {'username': username})
+    # userFound = response['success']
     userFound = True
     if userFound:
         msg = render_template('welcome_user')
+        #client.username = username
     else:
         msg = render_template('user_not_found')
     return question(msg)
